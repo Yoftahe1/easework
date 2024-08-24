8f57ebb6212bccb9309ba71040b63627ef5dc3b8
Yoftahe Merkebu <yoftahemerkebu2@gmail.com>
2024-08-24 17:04:30 +0300

    used alarm instead of setinterval

--- public/background.js
+++ public/background.js
@@ -1,16 +1,18 @@
-let intervalId = null;
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.action === "start") {
+    chrome.alarms.create('autoRefresh', { periodInMinutes: 1 });
+  } else if (message.action === "stop") {
+    chrome.alarms.clear('autoRefresh');
+  }
+});
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.action === "start" && !intervalId) {
-    intervalId = setInterval(() => {
-      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        if (tabs[0] && tabs[0].url.startsWith("https://www.upwork.com/nx/search/jobs")) {
-          chrome.tabs.reload(tabs[0].id);
-        }
-      });
-    }, 60000);
-  } else if (message.action === "stop" && intervalId) {
-    clearInterval(intervalId);
-    intervalId = null;
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+  if (alarm.name === 'autoRefresh') {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0] && tabs[0].url.startsWith("https://www.upwork.com/nx/search/jobs")) {
+        chrome.tabs.reload(tabs[0].id);
+      }
+    });
   }
 });
